7446eb611c340bcfb319a663c1059ac919387379
tristan <69066026+twisttaan@users.noreply.github.com>
2022-07-17 12:02:28 +1000

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html data-theme="evie">
+    <Html data-theme="imageing">
       <Head />
       <Main />
       <NextScript />
