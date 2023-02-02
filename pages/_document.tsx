c68c84c032eda5aa8c6a161c65827e5be4b10e30
thehairy <soeren.stabenow@outlook.com>
2023-02-02 16:55:39 +0100

    feat: favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html data-theme="imageing">
-      <Head />
+      <Head>
+		  <link rel="shortcut icon" href="/assets/favicon.ico" />
+	  </Head>
       <Main />
       <NextScript />
     </Html>
